101679827fba42621db55689a972b966a39bdd4d
qubitrenagade <qubitrenegade@gmail.com>
2019-10-06 13:13:27 -0600

    update to 1.6.1

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=qbrd
 pkg_name=consul
-pkg_version=1.2.2
-pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_version=1.6.1
+pkg_maintainer='Qubitrenegade <qubitrenegade@protonmail.com>'
 pkg_license=("MPL-2.0")
 pkg_description="Consul is a tool for service discovery, monitoring and configuration."
 pkg_upstream_url=https://www.consul.io/
 pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
-pkg_shasum=7fa3b287b22b58283b8bd5479291161af2badbc945709eb5412840d91b912060
+pkg_shasum=a8568ca7b6797030b2c32615b4786d4cc75ce7aee2ed9025996fe92b07b31f7e
 pkg_filename="${pkg_name}-${pkg_version}_linux_amd64.zip"
 pkg_deps=()
 pkg_build_deps=(core/unzip)
@@ -18,10 +18,13 @@ pkg_exports=(
   [port-serf_wan]=ports.serf_wan
   [port-server]=ports.server
 )
-pkg_exposes=(port-dns port-http port-serf_lan port-serf_wan port-server)
-
-pkg_svc_user="hab"
-pkg_svc_group="${pkg_svc_user}"
+pkg_exposes=(
+  port-dns
+  port-http
+  port-serf_lan
+  port-serf_wan
+  port-server
+)
 
 do_unpack() {
   cd "${HAB_CACHE_SRC_PATH}" || exit
---
+++ tests/helpers.bash
@@ -0,0 +1,44 @@
+# Usage: test_listen <protocol> <port> [wait-duration]
+# protocol: tcp or udp
+# port: int
+# wait-duration: time in seconds
+
+get_ip_address() {
+  ip route get 1 | awk '{print $NF;exit}'
+}
+
+check_ip() {
+  if [ -z "${1}" ]
+  then
+    get_ip_address
+  else
+    echo "${1}"
+  fi
+}
+
+test_listen() {
+  local proto="-z"
+  if [ "${1}" == "udp" ]; then
+    proto="-u"
+  fi
+  local ip_address
+  ip_address="$(check_ip "${4}")"
+
+  local wait="${3:-3}"
+  nc "${proto}" -w"${wait}" "${ip_address}" "${2}"
+  return $?
+}
+
+wait_listen() {
+  echo "${4}"
+  local proto="-z"
+  if [ "${1}" == "udp" ]; then
+    proto="-u"
+  fi
+  local ip_address
+  ip_address="$(check_ip "${4}")"
+  local wait=${3:-1}
+  while ! nc "${proto}" -w"${wait}" "${ip_address}" "${2}"; do
+    sleep 1
+  done
+}
